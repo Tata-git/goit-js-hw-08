14f8702fe412b7810000521c931986cc00a71315
Tata-git <83451272+Tata-git@users.noreply.github.com>
2022-05-24 19:45:51 +0000

    Deploying to gh-pages from @ Tata-git/goit-js-hw-08@697719805c86f9e20c2681393677b8b2129c5cad 🚀

--- 01-gallery.42da00e1.js
+++ 01-gallery.42da00e1.js
@@ -8,4 +8,4 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"TfQ5":[function(require,module,exports) {
 "use strict";var e=i(require("simplelightbox"));require("simplelightbox/dist/simple-lightbox.min.css");var l=require("./gallery-items");function i(e){return e&&e.__esModule?e:{default:e}}console.log(l.galleryItems);const t=document.querySelector(".gallery");function a(e){return e.map(({description:e,original:l,preview:i})=>`<div class="gallery__item">\n      <a class="gallery__item" href="${l}">\n  <img class="gallery__image" src="${i}" alt="${e}" />\n</a>;\n\t    </div>`).join("")}console.log(t),t.insertAdjacentHTML("beforeend",a(l.galleryItems)),new e.default(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250});
 },{"simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M","./gallery-items":"q5Rn"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/goit-js-hw-08/01-gallery.8e374e44.js.map
+//# sourceMappingURL=/goit-js-hw-08/01-gallery.42da00e1.js.map
